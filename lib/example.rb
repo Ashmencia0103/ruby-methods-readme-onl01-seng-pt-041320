21f0ad597f63e1a5400a4f5531c2c445cc3f18e5
Ashlen Mencia <ashmencia0103@gmail.com>
2020-04-14 20:00:21 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,6 @@
-
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
- 
+def greeting
+  puts "Hello World!"
+  end 
 
 
 
